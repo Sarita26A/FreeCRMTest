ce1b9fcb702535dbd15b675d1341214c1af2e69f
Sarita <sarita_25a@yahoo.co.in>
2022-11-17 13:35:24 -0500

    SearchPage Added and SignUp Page modified

---
+++ src/main/java/com/crm/qa/pages/SearchPage.java
@@ -0,0 +1,7 @@
+package com.crm.qa.pages;
+
+public class SearchPage {
+    public void searchPage(){
+        System.out.printf("Search Page");
+    }
+}
--- src/main/java/com/crm/qa/pages/SignUpPage.java
+++ src/main/java/com/crm/qa/pages/SignUpPage.java
@@ -3,4 +3,7 @@ package com.crm.qa.pages;
 import com.crm.qa.base.TestBase;
 
 public class SignUpPage extends TestBase {
+    public void signUpPage(){
+        System.out.println("Hello");
+    }
 }
